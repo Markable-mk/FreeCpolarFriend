54e40930412466bf6815d5afd7065597be4af568
makuan <2777451153@qq.com>
2024-09-20 13:18:44 +0800

    feat 支持crone表达式

--- src/main/java/com/itmark/job/config/QuartzConfig.java
+++ src/main/java/com/itmark/job/config/QuartzConfig.java
@@ -5,9 +5,13 @@ import com.itmark.enums.QuartzTypeEnum;
 import com.itmark.job.cpolar.CpolarJob;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
+import org.quartz.impl.triggers.CronTriggerImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
 
 /**
  * @description: 定义任务描述和具体的执行时间
@@ -24,6 +28,9 @@ public class QuartzConfig {
     @Value("${task-cycle.length:1}")
     private Integer length;
 
+    @Value("${task-cycle.cronExpression}")
+    private String cronExpression;
+
     @Bean
     public JobDetail jobDetail() {
         // 指定任务描述具体的实现类
@@ -39,7 +46,6 @@ public class QuartzConfig {
 
     @Bean
     public Trigger trigger() {
-        log.info("触发器循环时间类型：{}，时间长度：{}。", taskType, length);
         QuartzTypeEnum quartzTypeEnum = QuartzTypeEnum.getQuartzTypeEnum(taskType);
         SimpleScheduleBuilder simpleScheduleBuilder = null;
 
@@ -58,14 +64,24 @@ public class QuartzConfig {
                 simpleScheduleBuilder = SimpleScheduleBuilder.repeatHourlyForever(CpolarConstant.ONE);
                 break;
         }
-
-        // 创建触发器
-        SimpleTrigger buildTrigger = TriggerBuilder.newTrigger()
+        // 优先使用crone表达式触发器
+        if (!StringUtils.isEmpty(cronExpression)){
+            CronTrigger cronTrigger = TriggerBuilder.newTrigger()
+                    .withIdentity("cronTrigger1")
+                    .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression))
+                    .forJob(jobDetail())
+                    .build();
+            log.info("Cron表达式：{}，CronTrigger触发器初始化完毕。",cronExpression);
+            return cronTrigger;
+        }
+        // 次用循环触发器
+        SimpleTrigger simpleTrigger = TriggerBuilder.newTrigger()
+                .withIdentity("simpleTrigger1")
                 // 绑定工作任务
                 .forJob(jobDetail())
                 .withSchedule(simpleScheduleBuilder)
                 .build();
-        log.info("触发器初始化完毕。");
-        return buildTrigger;
+        log.info("触发器循环时间类型：{}，时间长度：{}，SimpleTrigger触发器初始化完毕。",taskType, length);
+        return simpleTrigger;
     }
 }
